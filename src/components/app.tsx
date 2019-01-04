d5011a73117f6041203525abb9242f29fb9b938f
kevinegstorf <kevin.egstorf@gmail.com>
2019-01-04 11:36:58 +0100

    first commit

---
+++ src/components/app.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  font-size: 1.5em;
+  text-align: center;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  color: palevioletred;
+`;
+
+const App = () => {
+  return <Title>Hello</Title>;
+};
+
+export default App;
